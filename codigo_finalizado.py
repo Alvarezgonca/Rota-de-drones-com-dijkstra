f0ec18d3fcd288b0feb87a17f0c2fc36ef0a8bfb
Alvarezgonca <alvarezgonca@gmail.com>
2025-06-05 06:09:52 -0300

    Código pronto e corrigido

--- codigo_finalizado.py
+++ codigo_finalizado.py
@@ -2,99 +2,128 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+# Mapeamento das letras para os nomes
+mapa_nomes = {
+    "A": "Araçatiba",
+    "B": "Bombeiro",
+    "C": "Cachoeira",
+    "D": "C.P.N",
+    "E": "C.C",
+    "F": "Centro",
+    "G": "Galpão Tech",
+    "H": "G.Spar",
+    "I": "Hosp. Theka",
+    "J": "Hosp. Conde",
+    "K": "Lagoa",
+    "L": "Max",
+    "M": "Moderna",
+    "N": "Museu",
+    "O": "Orla",
+    "P": "Pacheco",
+    "Q": "P.N",
+    "R": "Popular",
+    "S": "PS Inoa",
+    "T": "PS Maricá",
+    "U": "Praça",
+    "V": "Praia",
+    "W": "Praia P.N",
+    "X": "Prefeitura",
+    "Y": "Raia",
+    "Z": "Tamoio",
+    "AA": "Ultrafarma",
+    "AB": "UPA 1",
+    "AC": "UPA 2",
+    "AD": "UPA 3",
+}
+
+mapa_numerico = {
+    1: "Araçatiba",
+    2: "Bombeiro",
+    3: "Cachoeira",
+    4: "C.P.N",
+    5: "C.C",
+    6: "Centro",
+    7: "Galpão Tech",
+    8: "G.Spar",
+    9: "Hosp. Theka",
+    10: "Hosp. Conde",
+    11: "Lagoa",
+    12: "Max",
+    13: "Moderna",
+    14: "Museu",
+    15: "Orla",
+    16: "Pacheco",
+    17: "P.N",
+    18: "Popular",
+    19: "PS Inoa",
+    20: "PS Maricá",
+    21: "Praça",
+    22: "Praia",
+    23: "Praia P.N",
+    24: "Prefeitura",
+    25: "Raia",
+    26: "Tamoio",
+    27: "Ultrafarma",
+    28: "UPA 1",
+    29: "UPA 2",
+    30: "UPA 3",
+}
+
+def renomear(vertice):
+    return mapa_nomes.get(vertice, vertice)
+
 G = nx.Graph()
-G.add_edge("A", "G", weight=6)
-G.add_edge("B", "C", weight=7)
-G.add_edge("A", "C", weight=8)
-G.add_edge("D", "E", weight=7)
-G.add_edge("E", "F", weight=7)
-G.add_edge("E", "C", weight=7)
-G.add_edge("B", "D", weight=8)
-G.add_edge("A", "F", weight=6)
-G.add_edge("C", "H", weight=6)
-G.add_edge("G", "H", weight=7)
-G.add_edge("B", "I", weight=6)
-G.add_edge("I", "H", weight=9)
-G.add_edge("D", "J", weight=7)
-G.add_edge("K", "J", weight=7)
-G.add_edge("K", "E", weight=8)
-G.add_edge("K", "L", weight=7)
-G.add_edge("L", "F", weight=7)
-G.add_edge("L", "M", weight=6)
-G.add_edge("N", "M", weight=7)
-G.add_edge("N", "K", weight=6)
-G.add_edge("N", "O", weight=7)
-G.add_edge("O", "J", weight=5)
-G.add_edge("G", "P", weight=9)
-G.add_edge("P", "Q", weight=7)
-G.add_edge("Q", "A", weight=7)
-G.add_edge("Q", "R", weight=7)
-G.add_edge("R", "F", weight=8)
-G.add_edge("R", "S", weight=7)
-G.add_edge("S", "L", weight=7)
-G.add_edge("S", "T", weight=7)
-G.add_edge("T", "M", weight=8)
+arestas = [
+    ("A", "G", 6), ("B", "C", 7), ("A", "C", 8), ("D", "E", 7), ("E", "F", 7),
+    ("E", "C", 7), ("B", "D", 8), ("A", "F", 6), ("C", "H", 6), ("G", "H", 7),
+    ("B", "I", 6), ("I", "H", 9), ("D", "J", 7), ("K", "J", 7), ("K", "E", 8),
+    ("K", "L", 7), ("L", "F", 7), ("L", "M", 6), ("N", "M", 7), ("N", "K", 6),
+    ("N", "O", 7), ("O", "J", 5), ("G", "P", 9), ("P", "Q", 7), ("Q", "A", 7),
+    ("Q", "R", 7), ("R", "F", 8), ("R", "S", 7), ("S", "L", 7), ("S", "T", 7),
+    ("T", "M", 8), ("T", "U", 1), ("S", "U", 1), ("R", "U", 1), ("Q", "U", 1),
+    ("P", "U", 1), ("I", "V", 1), ("B", "V", 1), ("D", "V", 1), ("J", "V", 1),
+    ("O", "V", 1), ("V", "W", 1), ("V", "X", 1), ("V", "Y", 1), ("V", "Z", 1),
+    ("Z", "X", 1), ("X", "W", 1), ("W", "Y", 1), ("U", "AA", 1), ("U", "AB", 1),
+    ("U", "AC", 1), ("U", "AD", 1), ("AA", "AB", 1), ("AB", "AC", 1), ("AC", "AD", 1),
+]
+for u, v, w in arestas:
+    G.add_edge(renomear(u), renomear(v), weight=w)
 
-G.add_edge("T", "U", weight=1)
-G.add_edge("S", "U", weight=1)
-G.add_edge("R", "U", weight=1)
-G.add_edge("Q", "U", weight=1)
-G.add_edge("P", "U", weight=1)
-# I B D J O
-G.add_edge("I", "V", weight=1)
-G.add_edge("B", "V", weight=1)
-G.add_edge("D", "V", weight=1)
-G.add_edge("J", "V", weight=1)
-G.add_edge("O", "V", weight=1)
+# Dijkstra
+print("Locais disponíveis:")
+for num, nome in mapa_numerico.items():
+    print(f"{num}: {nome}")
+input_source = int(input("Digite o número do local de origem (1-30): "))
+input_target = int(input("Digite o número do local de destino (1-30): "))
 
-G.add_edge("V", "W", weight=1)
-G.add_edge("V", "X", weight=1)
-G.add_edge("V", "Y", weight=1)
-G.add_edge("V", "Z", weight=1)
+source_nome = mapa_numerico[input_source]
+target_nome = mapa_numerico[input_target]
 
-G.add_edge("Z", "X", weight=1)
-G.add_edge("X", "W", weight=1)
-G.add_edge("W", "Y", weight=1)
+source = renomear(source_nome)
+target = renomear(target_nome)
+caminho = nx.dijkstra_path(G, source=source, target=target, weight="weight")
 
-G.add_edge("U", "AA", weight=1)
-G.add_edge("U", "AB", weight=1)
-G.add_edge("U", "AC", weight=1)
-G.add_edge("U", "AD", weight=1)
-
-G.add_edge("AA", "AB", weight=1)
-G.add_edge("AB", "AC", weight=1)
-G.add_edge("AC", "AD", weight=1)
-
-caminho = nx.dijkstra_path(G, source="I", target="AD", weight="weight")
-print("Menor caminho de A até AD:", caminho)
-
+# Layout
 pos = nx.spring_layout(G, seed=42)
 
-
+# Animação
 arestas_do_caminho = list(zip(caminho, caminho[1:]))
-
-
-labels = nx.get_edge_attributes(G, 'weight')
-
-pos = nx.spring_layout(G, seed=42)
-
 fig, ax = plt.subplots()
 
-# Desenha o grafo todo de fundo (nós e arestas cinzas)
+# Desenho inicial do grafo
 cores_nos = []
 for node in G.nodes:
-    if node == "I" or node == "AD":
-        cores_nos.append("lightgreen")  # origem e destino
+    if node == source or node == target:
+        cores_nos.append("lightgreen")
+    elif node in caminho:
+        cores_nos.append("yellow")
     else:
-        cores_nos.append("lightgray")  # os demais
+        cores_nos.append("lightgray")
 nx.draw(G, pos, ax=ax, with_labels=True, node_color=cores_nos, edge_color="gray")
-
-
-# Rótulos de peso das arestas
 labels = nx.get_edge_attributes(G, 'weight')
 nx.draw_networkx_edge_labels(G, pos, edge_labels=labels, ax=ax)
 
-
+# Animação
 def atualizar(frame):
     if frame < len(arestas_do_caminho):
         edge = arestas_do_caminho[frame]
@@ -105,12 +134,12 @@ def atualizar(frame):
             width=4,
             ax=ax
         )
-ani = animation.FuncAnimation(
-    fig,              # figura que vai ser animada
-    atualizar,        # função chamada a cada frame
-    frames=len(arestas_do_caminho),  # número de etapas
-    interval=500,     # tempo entre uma aresta e outra (milissegundos)
-    repeat=False      # não repetir a animação
-)
-plt.show()
 
+ani = animation.FuncAnimation(
+    fig, atualizar,
+    frames=len(arestas_do_caminho),
+    interval=500,
+    repeat=False
+)
+
+plt.show()
